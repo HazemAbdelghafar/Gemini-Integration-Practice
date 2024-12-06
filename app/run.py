80559c01ffa44d6fee1738d55fa3382f75e0ec67
Hazem Abdelghafar <hazem.metwalli23@gmail.com>
2024-12-06 22:59:53 +0200

    Fix app startup sequence to run migrations before starting the server

--- app/run.py
+++ app/run.py
@@ -11,5 +11,5 @@ def start_app():
     os.execvp("uvicorn", ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"])
 
 if __name__ == "__main__":
-    run_migrations()
     start_app()
+    run_migrations()
